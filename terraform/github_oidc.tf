16781e67c69fe40d1ac2a7749a2393e82db0010b
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2026-08-10 12:06:05 -0700

    Fix OIDC trust policy: use job_workflow_ref instead of sub, scoped to deploy.yml on main

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -21,10 +21,8 @@ resource "aws_iam_role" "github_actions_deploy" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-        }
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:Chukwudalu/studymate:*"
+          "token.actions.githubusercontent.com:aud"             = "sts.amazonaws.com"
+          "token.actions.githubusercontent.com:job_workflow_ref" = "Chukwudalu/studymate/.github/workflows/deploy.yml@refs/heads/main"
         }
       }
     }]
