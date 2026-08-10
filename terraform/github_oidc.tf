6b4466e14b82ceaf280380eff2f0e7c6d67c266c
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2026-08-10 11:08:03 -0700

    Add GitHub Actions OIDC provider/role for AWS auth, plus a throwaway workflow to verify it

---
+++ terraform/github_oidc.tf
@@ -0,0 +1,32 @@
+data "tls_certificate" "github_actions" {
+    url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "studymate-github-actions-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github_actions.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:Chukwudalu/studymate:*"
+        }
+      }
+    }]
+  })
+}
